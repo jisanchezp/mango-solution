a7af23ad47901ff969a365af4e09c884809d6921
Juan I. Sánchez <juanisanchez@outlook.com>
2024-10-25 21:27:34 -0300

    Add Coupon & CouponDTO

---
+++ Mango.Services.Coupon.API/Models/Coupon.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.Services.Coupon.API.Models
+{
+    public class Coupon
+    {
+        public int CouponId { get; set; }
+        public string CouponName { get; set; }
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
