7940b7b9613293cb02e666cd32154d37e62a7e21
Juan I. Sánchez <juanisanchez@outlook.com>
2024-10-31 18:25:22 -0300

    Add Request and Response DTO
    Added request and response dto's in frontend
    MVC project. Added HttpVerb enum.

---
+++ Mango.Frontend.MVC/Enums/HttpVerbEnum.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Mango.Frontend.MVC.Enums
+{
+    public enum HttpVerbEnum : int
+    {
+        GET,
+        POST,
+        PUT,
+        DELETE
+    }
+}
---
+++ Mango.Frontend.MVC/Models/RequestDto.cs
@@ -0,0 +1,12 @@
+﻿using Mango.Frontend.MVC.Enums;
+
+namespace Mango.Frontend.MVC.Models
+{
+    public class RequestDto
+    {
+        public HttpVerbEnum HttpVerb { get; set; } = HttpVerbEnum.GET;
+        public string Url { get; set; } = string.Empty;
+        public object DiscountAmount { get; set; } = string.Empty;
+        public string AccessToken { get; set; } = string.Empty;
+    }
+}
