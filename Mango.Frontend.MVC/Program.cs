255abea4090ee01eb4d9fd98d2701343420ea06b
Juan I. Sánchez <juanisanchez@outlook.com>
2024-10-31 20:24:28 -0300

    Register services
    Registered httpClient, base and coupon services.

--- Mango.Frontend.MVC/Program.cs
+++ Mango.Frontend.MVC/Program.cs
@@ -1,15 +1,21 @@
+using Mango.Frontend.MVC.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddHttpClient();
+builder.Services.AddHttpClient<ICouponService, CouponService>();
+
+builder.Services.AddScoped<IBaseService, BaseService>();
+builder.Services.AddScoped<ICouponService, CouponService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
