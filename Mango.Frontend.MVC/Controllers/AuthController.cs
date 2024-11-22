65d348f50c5b0668a0e4bd28153a2d0f51d6e2a3
Juan I. Sánchez <juanisanchez@outlook.com>
2024-11-22 13:18:51 -0300

    Auth Controller MVC
    Added MVC Auth Controller and placeholder
    actions. Added nav links to those actions in layout.

---
+++ Mango.Frontend.MVC/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Mango.Frontend.MVC.Models.Dtos;
+using Mango.Frontend.MVC.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Frontend.MVC.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDto loginRequestDto = new();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
