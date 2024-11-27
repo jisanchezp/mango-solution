42a8e76d6fbd8fce7bddaffc98ca3b94a199a48e
Juan I. Sánchez <juanisanchez@outlook.com>
2024-11-27 18:21:59 -0300

    Configuring DB
    Configure SQL Server Database. Added
    migration to add products table. Added
    connection string.

--- Mango.Services.ProductAPI/Program.cs
+++ Mango.Services.ProductAPI/Program.cs
@@ -1,14 +1,19 @@
+using Mango.Services.ProductAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ProductsDb"));
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
